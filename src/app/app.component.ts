bc62495a7ed3b7d1b1d49f47256d91385eee0b13
Bonevy BEBY <bonevybeby@gmail.com>
2022-10-22 22:33:06 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+//import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, UntypedFormGroup, UntypedFormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Academind';
+  //form:any= new FormGroup({
+     //email: new FormControl<string|null>(null),
+    // age: new FormControl<number|null>(null),
+
+    form= new UntypedFormGroup({
+     email: new UntypedFormControl(null),
+     age: new UntypedFormControl(null),
+
+  });
+
+  onSubmit(){
+     if(this.form.value.email){
+       console.log(this.form.value.length);
+     }
+  }
 }
